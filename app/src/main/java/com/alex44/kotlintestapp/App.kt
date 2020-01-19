ffbf4acb1ec5fec1825c9809b90a8571cee7cbe8
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-19 15:01:45 +0500

    Инициализация Paper

--- app/src/main/java/com/alex44/kotlintestapp/App.kt
+++ app/src/main/java/com/alex44/kotlintestapp/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.alex44.kotlintestapp.di.AppComponent
 import com.alex44.kotlintestapp.di.DaggerAppComponent
 import com.alex44.kotlintestapp.di.modules.AppModule
+import io.paperdb.Paper
 import timber.log.Timber
 
 class App : Application() {
@@ -18,6 +19,7 @@ class App : Application() {
         super.onCreate()
         instance = this
         Timber.plant(Timber.DebugTree())
+        Paper.init(this);
         appComponent = DaggerAppComponent.builder()
             .appModule(AppModule(this))
             .build()
